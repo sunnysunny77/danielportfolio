9f95732a5bce01a632b0b1d5e541f85997ca1b20
daniel <contractor@lateralaspect.com.au>
2024-09-08 10:31:57 +0800

    UP

--- site/index.php
+++ site/index.php
@@ -11,7 +11,7 @@
     <title>Portfolio - Work</title>
     <link href="./css/app.min.css" rel="stylesheet" type="text/css" />
     <link rel="manifest" href="manifest.json" />
-    <link rel="apple-touch-icon" href="images/pwa-logo-small.png" />
+    <link rel="apple-touch-icon" href="images/pwa-logo-small.webp" />
 </head>
 
 <body>
