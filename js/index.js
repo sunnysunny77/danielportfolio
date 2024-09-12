c3f1cbeb45ae8056977899c5fb101161c3d070ad
daniel <contractor@lateralaspect.com.au>
2024-09-12 19:27:53 +0800

    fallback

--- js/index.js
+++ js/index.js
@@ -16,10 +16,12 @@ import { events } from "./utillites.js";
 import { copy_tooltip } from "./copy_tooltip.js";
 import { nav } from "./nav.js";
 import { has_move } from "./has_move.js";
+import { fallback } from "./fallback.js";
 
 events(window, "load", () => {
 
   AOS.init({once: true});
+  fallback();
   has_move();
   nav();
   copy_tooltip();
