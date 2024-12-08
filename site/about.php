8f5c02b4187b655b864f03f697d6d953ef4e32c1
daniel <contractor@lateralaspect.com.au>
2024-12-08 17:46:04 +0800

    Update about.php

--- site/about.php
+++ site/about.php
@@ -186,6 +186,9 @@
 
                                         <a class="mb-3 d-block" target="Sliders" href="https://sliders.sunnyhome.site/"> Sliders </a>
 
+                                        <a class="mb-3 d-block" target="Candid" href="https://candidcleaning.sunnyhome.site/"> Candid </a>
+
+
                                     </div>
 
                                     <div class="col-24 col-md-48 order-1 order-md-2">
