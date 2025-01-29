1c0943692645901a42775a1cf8a2071be0f48edd
daniel <contractor@lateralaspect.com.au>
2025-01-29 12:54:40 +0800

    Update init.js

--- js/init.js
+++ js/init.js
@@ -71,7 +71,7 @@ export const init = () => {
       AOS.init({
         once: true,
         easing: "ease-out-quad",
-        offset: intro_obj.id  === "intro-work" ? -800 : 200,
+        offset: intro_obj.id  === "intro-work" ? -800 : 100,
       });
     };
   });
