c3c9411e67a396c5814cedc5e2dabad692821e5e
daniel <contractor@lateralaspect.com.au>
2025-02-01 11:58:11 +0800

    Update date.js

--- js/date.js
+++ js/date.js
@@ -1,11 +1,16 @@
-const time = () => {
-
-  const date = new Date();
-  const time = date.toLocaleTimeString("en-AU", { timeZone: "Australia/Perth", hour: "numeric", minute: "2-digit" });
-  document.querySelector("#date").innerHTML = `${time} &mdash; Perth`;
-};
-
 export const date = () => {
 
-  setInterval(time, 1000);
+  const obj = document.querySelector("#date");
+
+  if (!obj) {
+
+    return;
+  }
+
+  setInterval(() => {
+
+    const date = new Date();
+    const time = date.toLocaleTimeString("en-AU", { timeZone: "Australia/Perth", hour: "numeric", minute: "2-digit" });
+    obj.innerHTML = `${time} &mdash; Perth`;
+  }, 1000);
 };
