8f04086562ba11396a396a6ad1a8d4441667fe44
daniel <contractor@lateralaspect.com.au>
2025-04-11 15:21:09 +0800

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -6,7 +6,6 @@ export const nav = () => {
     const button = document.querySelector(".nav-tooltip");
     const main = document.querySelector("main");
 
-
     if (!nav || !button || !main ) {
 
       return;
@@ -18,8 +17,6 @@ export const nav = () => {
 
     let positive = false;
 
-    let style = {};
-
     const handle_nav = () => {
 
       const main_top = main.offsetTop;
@@ -28,14 +25,22 @@ export const nav = () => {
 
       const nav_top = nav.getBoundingClientRect().height;
 
-      let scroll_pos = window.scrollY;
+      const scroll_pos = window.scrollY;
 
-      let obj = {};
+      const obj = {
+        position: "absolute",
+        zIndex: "999",
+        top: "0px",
+        transition: "none",
+        maxHeight: "",
+        backgroundColor: "transparent",
+        padding: "60px 0",
+      };
 
       if (window.innerWidth > 992) {
 
         obj.position = "absolute";
-        obj.zIndex = 999;
+        obj.zIndex = "999";
         obj.top = "0px";
         obj.transition = "none";
         obj.maxHeight = "";
@@ -50,7 +55,7 @@ export const nav = () => {
       if (scroll_pos < main_top) {
 
         obj.position = "absolute";
-        obj.zIndex = 999;
+        obj.zIndex = "999";
         obj.top = "0px";
         obj.transition = "none";
         obj.maxHeight = "";
@@ -58,10 +63,19 @@ export const nav = () => {
         obj.padding = "60px 0";
         button.classList.remove("has-fixed");
         button.classList.remove("has-display");
+      } else if (scroll_pos > main_top && scroll_pos < main_top + nav_top && positive) {
+
+        obj.position = "fixed";
+        obj.zIndex = "997";
+        obj.top = `-${height}px`;
+        obj.transition = "none";
+        obj.maxHeight = "0px";
+        obj.backgroundColor = "rgb(23, 34, 43)";
+        obj.padding = "15px 0 5px 0";
       } else if ((scroll_pos > main_top + nav_top && positive) || (scroll_pos > main_top && scroll_pos < main_top + nav_top && !positive) || (scroll_pos > main_bottom)) {
 
         obj.position = "fixed";
-        obj.zIndex = 997;
+        obj.zIndex = "997";
         obj.top = `-${height}px`;
         obj.transition = "top 0.375s, max-height 1s, background-color 1s";
         obj.maxHeight = "0px";
@@ -70,7 +84,7 @@ export const nav = () => {
       } else if (scroll_pos > main_top + nav_top && !positive) {
 
         obj.position = "fixed";
-        obj.zIndex = 997;
+        obj.zIndex = "997";
         obj.top = "0px";
         obj.transition = "top 0.375s, max-height 1s, background-color 1s";
         obj.maxHeight = `${height}px`;
@@ -79,7 +93,7 @@ export const nav = () => {
         button.classList.add("has-fixed");
       }
       
-      if (obj !== style) Object.assign(nav.style, obj);
+      Object.assign(nav.style, obj);
 
       if (scroll_pos > scrollY) {
 
@@ -88,8 +102,6 @@ export const nav = () => {
 
         positive = false;
       }
-
-      style = obj;
       
       scrollY = window.scrollY;
     };
